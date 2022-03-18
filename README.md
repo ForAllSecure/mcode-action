de6b5a857e15dc8dbc7cb56139184a00c866b158
Andrew Yang <andrewyang@forallsecure.com>
2022-03-18 16:08:35 -0400

    releasing to public

---
+++ README.md
@@ -0,0 +1,139 @@
+# Mayhem for Code GitHub Action
+
+[![Mayhem for Code](https://drive.google.com/uc?export=view&id=1JXEbfCDMMwwnDaOgs5-XlPWQwZR93fv4)](http://mayhem.forallsecure.com/)
+
+A GitHub Action for using Mayhem for Code to check for reliability, performance and security issues in your application binary (packaged as a containerized [Docker](https://docs.docker.com/get-started/overview/) image).
+
+## About Mayhem for Code
+
+🧪 Modern App Testing: Mayhem for Code is an application security testing tool that catches reliability, performance and security bugs before they hit production.
+
+🧑‍💻 For Developers, by developers: The engineers building software are the best equipped to fix bugs, including security bugs. As engineers ourselves, we're building tools that we wish existed to make our job easier!
+
+🤖 Simple to Automate in CI: Tests belong in CI, running on every commit and PRs. We make it easy, and provide results right in your PRs where you want them. Adding Mayhem for Code to a DevOps pipeline is easy.
+
+Want to try it? [Get started for free](https://forallsecure.com/mayhem-free) today!
+
+## Getting Started
+
+To use the Mayhem for Code GitHub Action, perform the following steps:
+
+1. Create a Mayhem account and copy and paste your account token to GitHub Secrets.
+
+    a. Navigate to [mayhem.forallsecure.com](https://mayhem.forallsecure.com/) to register an account.
+
+    b. Click your profile drop-down and go to *Settings* > *API Tokens* to access your account API token.
+
+    c. Copy and paste your Mayhem token to your [GitHub Secrets](https://docs.github.com/en/actions/security-guides/encrypted-secrets#creating-encrypted-secrets-for-an-organization).
+
+2. Create a `mayhem.yml` file in your GitHub repository located at:
+
+    ```sh
+    .github/workflows/mayhem.yml
+    ```
+
+🤔 Still need some help? Take a look at our working mCode Action examples at: [https://github.com/forallsecure/mcode-action-examples](https://github.com/forallsecure/mcode-action-examples).
+
+## Usage
+
+Your `mayhem.yml` file should look like the following:
+
+```yaml
+name: Mayhem
+on:
+  pull_request:
+  workflow_dispatch:
+
+env:
+  REGISTRY: ghcr.io
+  IMAGE_NAME: ${{ github.repository }}
+
+jobs:
+  build:
+    name: '${{ matrix.os }} shared=${{ matrix.shared }} ${{ matrix.build_type }}'
+    runs-on: ${{ matrix.os }}
+    strategy:
+      matrix:
+        os: [ubuntu-latest]
+        shared: [false]
+        build_type: [Release]
+        include:
+          - os: ubuntu-latest
+            triplet: x64-linux
+
+    steps:
+      - uses: actions/checkout@v2
+
+      - name: Log in to the Container registry
+        uses: docker/login-action@f054a8b539a109f9f41c372932f1ae047eff08c9
+        with:
+          registry: ${{ env.REGISTRY }}
+          username: ${{ github.actor }}
+          password: ${{ secrets.GITHUB_TOKEN }}
+
+      - name: Extract metadata (tags, labels) for Docker
+        id: meta
+        uses: docker/metadata-action@98669ae865ea3cffbcbaa878cf57c20bbf1c6c38
+        with:
+          images: ${{ env.REGISTRY }}/${{ env.IMAGE_NAME }}
+
+      - name: Build and push Docker image
+        uses: docker/build-push-action@ad44023a93711e3deb337508980b4b5e9bcdc5dc
+        with:
+          context: .
+          push: true
+          tags: ${{ steps.meta.outputs.tags }}
+          labels: ${{ steps.meta.outputs.labels }}
+
+      - name: Start analysis
+        uses: ethan42/mcode-action@44343bdb4c774508c5b032f1cb24c805ccb5167e
+        with:
+          mayhem-token: ${{ secrets.MAYHEM_TOKEN }}
+          args: --image ${{ steps.meta.outputs.tags }} --corpus file://mayhem/corpus
+          sarif-output: sarif
+
+      - name: Upload SARIF file(s)
+        uses: github/codeql-action/upload-sarif@v1
+        with:
+          sarif_file: sarif
+```
+
+The mCode Action accepts the following inputs:
+
+| Required | Input Name | Type | Description | Default
+| --- | --- | --- | --- | ---
+| ✔️ | `mayhem-token` | string | Mayhem for Code account token |
+|   | `args` | string | Additional arguments such as specifying the `--corpus` directory path |
+|   | `sarif-output` | string | Path for generating a SARIF report output file |
+|   | `sarif_file` | string | Path for uploading a SARIF report input file to GitHub |
+
+## Reports and GitHub Code Scanning
+
+Mayhem for Code generates [SARIF reports](https://sarifweb.azurewebsites.net/#:~:text=The%20Static%20Analysis%20Results%20Interchange,approved%20as%20an%20OASIS%20standard.) for your application security testing results.
+
+SARIF reports are generated using the `sarif-output` parameter, which specifies an output file path. Similarly, to upload the SARIF report to GitHub, use the `sarif_file` parameter to specify the location of an input SARIF file to upload to GitHub.
+
+```sh
+- name: Start analysis
+  uses: ethan42/mcode-action@44343bdb4c774508c5b032f1cb24c805ccb5167e
+  with:
+    mayhem-token: ${{ secrets.MAYHEM_TOKEN }}
+    args: --image ${{ steps.meta.outputs.tags }} --corpus file://mayhem/corpus
+    sarif-output: sarif
+
+- name: Upload SARIF file(s)
+  uses: github/codeql-action/upload-sarif@v1
+  with:
+    sarif_file: sarif
+```
+
+Once uploaded to GitHub, you can view test results in the `Security` tab of your repository as well as your for your individual pull requests.
+
+![code-scanning-alert](code-scanning-alert.png)
+
+## About Us
+
+ForAllSecure was founded with the mission to make the world’s critical software safe. The company has been applying its patented technology from over a decade of CMU research to solving the difficult challenge of making software safer. ForAllSecure has partnered with Fortune 1000 companies in aerospace, automotive and high-tech industries, as well as the US Department of Defense to integrate Mayhem into software development cycles for continuous security. Profitable and revenue-funded, the company is scaling rapidly.
+
+* [https://forallsecure.com/](https://forallsecure.com/)
+* [https://forallsecure.com/mayhem-for-code](https://forallsecure.com/mayhem-for-code)
---
+++ src/main.ts
@@ -0,0 +1,167 @@
+import * as core from '@actions/core'
+import * as exec from '@actions/exec'
+import * as github from '@actions/github'
+import * as tc from '@actions/tool-cache'
+import {readFileSync, chmodSync} from 'fs'
+
+// Return local path to donwloaded or cached CLI
+async function mcodeCLI(): Promise<string> {
+  // Get latest version from API
+  const cliVersion = 'latest'
+  const os = 'Linux'
+  const bin = 'mayhem'
+
+  // Return cache if available
+  const cachedPath = tc.find(bin, cliVersion, os)
+  if (cachedPath) {
+    core.debug(`found cache: ${cachedPath}`)
+    return `${cachedPath}/${bin}`
+  }
+
+  // Download the CLI and cache it if version is set
+  const mcodePath = await tc.downloadTool(
+    `https://mayhem.forallsecure.com/cli/${os}/${bin}`
+  )
+  chmodSync(mcodePath, 0o755)
+  const folder = await tc.cacheFile(mcodePath, bin, bin, cliVersion, os)
+  return `${folder}/${bin}`
+}
+
+async function run(): Promise<void> {
+  try {
+    // Disable auto udpates since we always get the latest CLI
+    process.env['SKIP_MAPI_AUTO_UPDATE'] = 'true'
+    const cli = await mcodeCLI()
+
+    // Load inputs
+    const mayhemUrl: string =
+      core.getInput('mayhem-url') || 'https://mayhem.forallsecure.com'
+    const githubToken: string | undefined = core.getInput('github-token')
+    const mayhemToken: string = core.getInput('mayhem-token', {required: true})
+    const sarifOutput: string = core.getInput('sarif-output') || ''
+    const args: string[] = (core.getInput('args') || '').split(' ')
+    // defaults next
+    if (!args.includes('--duration')) {
+      args.push('--duration', '30')
+    }
+    if (!args.includes('--image')) {
+      args.push('--image', 'forallsecure/debian-buster:latest')
+    }
+
+    // Auto-generate target name
+    const repo = process.env['GITHUB_REPOSITORY']
+    const account = repo?.split('/')[0].toLowerCase()
+    const project = repo?.split('/')[1].toLowerCase()
+    if (repo === undefined) {
+      throw Error(
+        'Missing GITHUB_REPOSITORY environment variable. Are you not running this in a Github Action environement?'
+      )
+    }
+    const event =
+      JSON.parse(
+        readFileSync(process.env['GITHUB_EVENT_PATH'] || 'event.json', 'utf-8')
+      ) || {}
+    const event_pull_request = event.pull_request
+    const ci_url = `${process.env['GITHUB_SERVER_URL']}:443/${repo}/actions/runs/${process.env['GITHUB_RUN_ID']}`
+    const branch_name = event_pull_request
+      ? event_pull_request.head.ref
+      : process.env['GITHUB_REF_NAME']?.slice('refs/heads/'.length) || 'main'
+    const revision = event_pull_request
+      ? event_pull_request.head.sha
+      : process.env['GITHUB_SHA'] || 'unknown'
+    const merge_base_branch_name = event_pull_request ? event_pull_request.base.ref : 'main'
+
+    args.push('--ci-url', ci_url)
+    args.push('--merge-base-branch-name', merge_base_branch_name)
+    args.push('--branch-name', branch_name)
+    args.push('--revision', revision)
+
+    const argsString = args.join(' ')
+    // decide on the application type
+
+    const script = `
+    set -x
+    if [ -n "${sarifOutput}" ]; then
+      mkdir -p ${sarifOutput};
+    fi
+    is_rust=$(cargo fuzz list);
+    if [ -n "$is_rust" ]; then
+      for fuzz_target in $is_rust; do
+        cargo fuzz build $fuzz_target;
+        for path in $(ls fuzz/target/*/*/$fuzz_target); do
+          ${cli} package $path -o $fuzz_target;
+          rm -rf $fuzz_target/root/lib;
+          [[ -e fuzz/corpus/$fuzz_target ]] && cp fuzz/corpus/$fuzz_target/* $fuzz_target/corpus/;
+          sed -i 's,project: .*,project: ${repo.toLowerCase()},g' $fuzz_target/Mayhemfile;
+          run=$(${cli} run $fuzz_target --corpus file://$(pwd)/$fuzz_target/corpus ${argsString});
+          if [ -n "${sarifOutput}" ]; then
+            ${cli} wait $run -n ${account} --sarif ${sarifOutput}/$fuzz_target.sarif;
+            run_number=$(echo $run | awk -F/ '{print $NF}')
+            curl -H 'X-Mayhem-Token: token ${mayhemToken}' ${mayhemUrl}/api/v1/namespace/${account}/project/${project}/target/$fuzz_target/run/$run_number > $fuzz_target.json
+          fi
+        done
+      done
+    else
+      sed -i 's,project: .*,project: ${repo.toLowerCase()},g' Mayhemfile;
+      fuzz_target=$(grep target: Mayhemfile | awk '{print $2}')
+      run=$(${cli} run . ${argsString});
+      if [ -n "${sarifOutput}" ]; then
+        ${cli} wait $run -n ${account} --sarif ${sarifOutput}/target.sarif;
+        run_number=$(echo $run | awk -F/ '{print $NF}')
+        curl -H 'X-Mayhem-Token: token ${mayhemToken}' ${mayhemUrl}/api/v1/namespace/${account}/project/${project}/target/$fuzz_target/run/$run_number > mayhem.json
+      fi
+    fi
+`
+    process.env['MAYHEM_TOKEN'] = mayhemToken
+    process.env['MAYHEM_URL'] = mayhemUrl
+    process.env['MAYHEM_PROJECT'] = repo
+
+    // Start fuzzing
+    const cliRunning = exec.exec('bash', ['-c', script], {
+      ignoreReturnCode: true
+    })
+    const res = await cliRunning
+    if (res !== 0) {
+      // TODO: should we print issues here?
+      throw new Error('The Mayhem for Code scan found issues in the Target')
+    }
+
+    if (githubToken !== undefined) {
+      const octokit = github.getOctokit(githubToken)
+      const context = github.context
+      const {pull_request} = context.payload
+      const output = JSON.parse(readFileSync('mayhem.json', 'utf-8')) || {}
+      core.info(`pull request ready: ${pull_request !== undefined}`)
+      if (pull_request !== undefined) {
+        await octokit.rest.issues.createComment({
+          ...context.repo,
+          issue_number: pull_request.number,
+          body: `# [Mayhem for Code](${mayhemUrl}) Report :warning:
+
+Merging [#${pull_request.number}](${pull_request.html_url}) ${pull_request.head.ref} (${pull_request.head.sha.slice(0,8)}) into ${pull_request.base.ref} (${pull_request.base.sha.slice(0,8)})
+
+## Active Defects: ${output.n_defects} :x:
+
+## Testing Iterations Performed: ${output.tests_run} (${output.cputime} CPU seconds)
+
+## Testing Inputs Stored: ${output.n_testcase_reports}
+
+## Dynamic Block Coverage: ${output.run_attributes.n_blocks_covered * 100.0 / output.run_attributes.n_blocks_total}%
+
+[Continue to view full report in Mayhem for Code](${mayhemUrl}/${repo})
+
+`
+        })
+      }
+      core.info(`token defined: ${githubToken !== undefined}`)
+    }
+
+  } catch (err: unknown) {
+    if (err instanceof Error) {
+      core.info(`mcode action failed with: ${err.message}`)
+      core.setFailed(err.message)
+    }
+  }
+}
+
+run()
