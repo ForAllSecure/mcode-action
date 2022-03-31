5a4366891098d88fe4988d9c24369e57e631b2bb
Andrew Yang <andrewyang@forallsecure.com>
2022-03-31 13:28:55 -0400

    adding error message for package visbility

--- src/main.ts
+++ src/main.ts
@@ -145,9 +145,9 @@ async function run(): Promise<void> {
     const res = await cliRunning;
     if (res == 1) {
       // TODO: should we print issues here?
-      throw new Error("The Mayhem for Code scan was unable to execute the Mayhem run for your target. Check your configuration.");
+      throw new Error("The Mayhem for Code scan was unable to execute the Mayhem run for your target. Check your configuration. For package visibility/permissions issues, see https://docs.github.com/en/packages/learn-github-packages/configuring-a-packages-access-control-and-visibility on how to set your package to `Public`.");
     } else if (res == 2) {
-      throw new Error("The Mayhem run for your target was unsuccessful.");
+      throw new Error("The Mayhem for Code scan detected that the Mayhem run for your target was unsuccessful.");
     }
 
     if (githubToken !== undefined) {
