d40bfe13b810bb9a51ecda4eb5d8aa53990074e4
Benjamin Gutierrez <ben.gutierrez@forallsecure.com>
2023-02-28 14:50:16 -0500

    if statement bug

--- src/main.ts
+++ src/main.ts
@@ -140,7 +140,7 @@ async function run(): Promise<void> {
     fi
 
     # download coverage (owner flag doesn't work for download)
-     if [ -z "${coverageOutput}" ]; then
+     if ! [ -z "${coverageOutput}" ]; then
        ${cli} --verbosity ${verbosity} download ${account}/$run -o ${coverageOutput}
      fi
 
