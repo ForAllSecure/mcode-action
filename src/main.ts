9308d6b2daa3aeb75564754f03da629b9951ae25
George Giatrakos <geo.giatrakos@gmail.com>
2023-12-19 16:29:24 +0200

    Extract target from run

--- src/main.ts
+++ src/main.ts
@@ -151,7 +151,8 @@ async function run(): Promise<void> {
             ${waitArgsString}; then
       exit 3;
     fi
-
+    
+    
     # check status, exit with non-zero status if failed or stopped
     status=$(${cli} --verbosity ${verbosity} show \
                     --owner ${owner} \
@@ -160,8 +161,10 @@ async function run(): Promise<void> {
       exit 2;
     fi
 
+    target=$(echo $run | cut -d'/' -f2)
+
     if [ -n "${coverageOutput}" ]; then
-      ${cli} --verbosity ${verbosity} download  --owner ${owner} $run -o ${coverageOutput};
+      ${cli} --verbosity ${verbosity} download  --owner ${owner} ${project}/$target -o ${coverageOutput};
     fi
     `;
 
