0d612ffd72fb24b0fdacb398f0d262bd2d392dca
Azizjon Rizayev <rizaziz2002@forallsecure.com>
2026-04-22 11:14:25 -0400

    Revert "Add retry to mayhem run"
    This reverts commit 365de8339d989e59b3e177e53f9a1c04124f37f7.

--- dist/index.js
+++ dist/index.js
@@ -143,39 +143,22 @@ function run() {
             process.env["MAYHEM_URL"] = mayhemUrl;
             process.env["MAYHEM_PROJECT"] = config.repo;
             // Start fuzzing
-            const attempts = 10;
-            let retry = 0;
-            const delay = 60000;
-            const factor = 2;
-            // eslint-disable-next-line no-constant-condition
-            while (true) {
-                const cliRunning = (0, exec_1.exec)("bash", ["-c", script], {
-                    ignoreReturnCode: true,
-                });
-                const res = yield cliRunning;
-                if (res === 0) {
-                    break;
-                }
-                else if (res === 1) {
-                    if (retry >= attempts) {
-                        throw new Error(`The Mayhem for Code scan was unable to execute the Mayhem run for your target.
-            Check your configuration. For package visibility/permissions issues, see
-            https://docs.github.com/en/packages/learn-github-packages/configuring-a-packages-access-control-and-visibility
-            on how to set your package to 'Public'.`);
-                    }
-                    else {
-                        retry++;
-                        setTimeout(() => { }, delay * Math.pow(factor, retry) + Math.random() * 1000);
-                    }
-                }
-                else if (res === 2) {
-                    throw new Error("The Mayhem for Code scan detected the Mayhem run for your " +
-                        "target was unsuccessful.");
-                }
-                else if (res === 3) {
-                    throw new Error("The Mayhem for Code scan found defects in your target.");
-                }
-                break;
+            const cliRunning = (0, exec_1.exec)("bash", ["-c", script], {
+                ignoreReturnCode: true,
+            });
+            const res = yield cliRunning;
+            if (res === 1) {
+                throw new Error(`The Mayhem for Code scan was unable to execute the Mayhem run for your target.
+      Check your configuration. For package visibility/permissions issues, see
+      https://docs.github.com/en/packages/learn-github-packages/configuring-a-packages-access-control-and-visibility
+      on how to set your package to 'Public'.`);
+            }
+            else if (res === 2) {
+                throw new Error("The Mayhem for Code scan detected the Mayhem run for your " +
+                    "target was unsuccessful.");
+            }
+            else if (res === 3) {
+                throw new Error("The Mayhem for Code scan found defects in your target.");
             }
         }
         catch (err) {
