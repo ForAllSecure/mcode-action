1e1c02474f142075a2875f959986227c2500f603
Azizjon Rizayev <rizaziz2002@forallsecure.com>
2026-05-07 14:09:31 +0000

    Use 2 minutes duration for the runs

--- src/main.ts
+++ src/main.ts
@@ -110,7 +110,7 @@ async function run(): Promise<void> {
     const args: string[] = (getInput("args") || "").split(" ");
 
     // defaults next
-    args.push("--duration", "14400");
+    args.push("--duration", "120");
 
     if (!args.includes("--image")) {
       args.push("--image", "forallsecure/debian-buster:latest");
